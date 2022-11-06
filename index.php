5e58daabfa6afcb6ceafa7bf7bce3becd8199b78
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-06 14:29:09 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    // Access Modifiers
+    // In the PHP programming language, every property and method of a class can have access modifiers which control their accessibility in the entire program. 
+
+    // There are three access modifiers in PHP:
+    ?>
+</body>
+
+</html>
